1a976a4859074d8802cf435d9d09e7e01a96b547
Sito8943 <sito8943@gmail.com>
2023-02-05 10:52:04 -0500

    using favorites

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// @fortawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as Full } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as Regular } from "@fortawesome/free-regular-svg-icons";
 
 const Card = (props) => {
   const { id, name, src } = props;
 
+  const [hover, setHover] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(() => {
+    try {
+      const favorites = localStorage.getItem("favorites");
+      if (favorites !== null) {
+        const favoritesAsObject = JSON.parse(favorites);
+        if (typeof favoritesAsObject === "object" && favoritesAsObject[id])
+          setIsFavorite(true);
+      }
+    } catch (err) {
+      console.error(err);
+      localStorage.setItem("favorites", JSON.stringify({}));
+    }
+  }, [id]);
+
+  const setThisAsFavorite = () => {
+    try {
+      const favorites = localStorage.getItem("favorites");
+      if (favorites !== null) {
+        const favoritesAsObject = JSON.parse(favorites);
+        if (typeof favoritesAsObject === "object") {
+          if (favoritesAsObject[id]) {
+            setIsFavorite(false);
+            favoritesAsObject[id] = false;
+          } else {
+            setIsFavorite(true);
+            favoritesAsObject[id] = true;
+          }
+          localStorage.setItem("favorites", JSON.stringify(favoritesAsObject));
+        } else {
+          const newFavorites = {};
+          newFavorites[id] = true;
+          localStorage.setItem("favorites", JSON.stringify(newFavorites));
+          setThisAsFavorite(true);
+        }
+      } else {
+        const newFavorites = {};
+        newFavorites[id] = true;
+        localStorage.setItem("favorites", JSON.stringify(newFavorites));
+        setThisAsFavorite(true);
+      }
+    } catch (err) {
+      console.error(err);
+      localStorage.setItem("favorites", JSON.stringify({}));
+    }
+  };
+
   return (
-    <div id={id} className="card">
+    <div
+      onMouseLeave={() => setHover(false)}
+      onMouseEnter={() => setHover(true)}
+      id={id}
+      className="card"
+    >
+      <button
+        onClick={setThisAsFavorite}
+        className={`favorite-button ${
+          hover ? "opacity-up" : "opacity-down"
+        } text-red`}
+      >
+        <FontAwesomeIcon icon={isFavorite ? Full : Regular} />
+      </button>
       <img src={src} alt={name} />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      red: "crimson",
+    },
     extend: {},
   },
   plugins: [],
